f4519152bad7d445be8943e17635733f27e6f391
Dale Race <dale.race@slalom.com>
2020-08-26 08:14:39 -0500

    update site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Wentzville Grade Viewer`,
+    description: `Nightly screenshots of kids grades`,
+    author: `@racedale`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
